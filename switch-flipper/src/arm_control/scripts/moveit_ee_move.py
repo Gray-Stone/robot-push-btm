619db95ed23cb8d572e02d555b696a93cbad0098
leo-chen <S8O9R6@u.northwestern.edu>
2024-02-07 13:10:23 -0600

    Tryed to fix issue with computIK command. nothing worked

--- switch-flipper/src/arm_control/scripts/moveit_ee_move.py
+++ switch-flipper/src/arm_control/scripts/moveit_ee_move.py
@@ -7,7 +7,7 @@ from rclpy.wait_for_message import wait_for_message
 from tf_transformations import euler_from_quaternion, quaternion_from_euler
 
 from std_msgs.msg import Header
-from geometry_msgs.msg import Pose as PoseMsg
+from geometry_msgs.msg import Pose as PoseMsg ,PoseStamped
 
 from moveit_msgs.srv import GetPositionIK as GetPositionIKSrv
 from moveit_msgs.msg import PositionIKRequest as PositionIKRequestMsg
@@ -18,15 +18,18 @@ from moveit_msgs.msg import OrientationConstraint as MoveitOrientationConstraint
 from moveit_msgs.msg import PositionConstraint as MoveitPositionConstraint
 
 from moveit_msgs.msg import BoundingVolume, MoveItErrorCodes
+from moveit_msgs.action import MoveGroup
 from shape_msgs.msg import SolidPrimitive
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.action import ActionClient
 
 import math
 
 
 class EEPlaner(RosNode):
 
-    EE_LINK_NAME = "lollypop_ee_link"
+    # EE_LINK_NAME = "lollypop_ee_link"
+    EE_LINK_NAME = "wx200/ee_gripper_link"
     BASE_LINK = 'base_link'
 
     def __init__(self):
@@ -52,12 +55,20 @@ class EEPlaner(RosNode):
         self.last_js = init_js
         self._timer_callback_group = MutuallyExclusiveCallbackGroup()
 
-        self._tf_cal_timer = self.create_timer(1, self.IkCmdTimer, self._timer_callback_group)
 
         self.compute_ik_service_ = self.create_client(
             GetPositionIKSrv,
             "compute_ik",
         )
+        self.move_action_client_ = ActionClient(
+            self,
+            MoveGroup,
+            "move_action",
+        )
+
+
+        self._tf_cal_timer = self.create_timer(1, self.IkCmdTimer, self._timer_callback_group)
+
 
     async def IkCmdTimer(self):
         # Home pose for lollypop_ee_link
@@ -65,24 +76,29 @@ class EEPlaner(RosNode):
         # Good mid air pos
 
         # x: 0.23154 y: 0 z: 0.27571
-
-        mid_air_pos = PoseMsg()
-        mid_air_pos.position.x = 0.23154
-        mid_air_pos.position.y = 0.01
-        mid_air_pos.position.z = 0.27571
-
+        # 0.2179430539986765 y: 0.0016716351397863933 z: 0.15803736261075846
+        mid_air_pos = PoseStamped()
+        mid_air_pos.pose.position.x = 0.2179430539986765
+        mid_air_pos.pose.position.y = 0.0016716351397863933
+        mid_air_pos.pose.position.z = 0.15803736261075846
+        # mid_air_pos.header.frame_id = self.BASE_LINK
+        # mid_air_pos.header.stamp = self.get_clock().now().to_msg()
+        mid_air_pos.header = self.GenBaseHeader()
         await self.ComputeIK(mid_air_pos)
 
     def JointStateCB(self, msg: JointState):
         self.last_js = msg
 
-    def GenEEHeader(self):
+    def GenBaseHeader(self):
         header = Header()
         header.frame_id = self.BASE_LINK
         header.stamp = self.get_clock().now().to_msg()
         return header
 
-    async def ComputeIK(self, target_pos: PoseMsg):
+    # async def MoveGroupPlan(self ,target_pos: PoseStamped ):
+
+
+    async def ComputeIK(self, target_pos: PoseStamped):
 
         # /compute_ik [moveit_msgs/srv/GetPositionIK]
         ik_req = PositionIKRequestMsg()
@@ -91,16 +107,22 @@ class EEPlaner(RosNode):
 
         ik_req.robot_state.joint_state = self.last_js
 
+        print("==================== Robot state")
+        print(ik_req.robot_state)
         # Give huge z axis tolerance, cuz it's a 5dof
-        ik_req.constraints.orientation_constraints = [self.GetOrientationConstrain()]
+        ik_req.constraints.name = self.EE_LINK_NAME
+        ik_req.constraints.orientation_constraints = [self.GetOrientationConstrain(target_pos)]
         ik_req.constraints.position_constraints = [self.GetPosConstrain(target_pos)]
-
+        ik_req.avoid_collisions=False
         response: GetPositionIKSrv.Response = await self.compute_ik_service_.call_async(
             GetPositionIKSrv.Request(ik_request=ik_req))
 
+        ik_req.pose_stamped = target_pos
         sol = response.solution
         err = response.error_code
 
+        print("==================== ik req")
+        print(ik_req)
         if err != MoveItErrorCodes.SUCCESS:
             print(f"Planning finished with error: \n{err}")
             return
@@ -108,11 +130,15 @@ class EEPlaner(RosNode):
         print(f"Planning succeed!")
         print(sol)
 
-    def GetOrientationConstrain(self, z_ang=0):
+        self._tf_cal_timer.cancel()
+
+    def GetOrientationConstrain(self ,pose: PoseStamped):
 
         # https://github.com/ros-planning/moveit_msgs/blob/0e203aaffd1f754ab3b31110eff0eaf26e1c3599/msg/OrientationConstraint.msg
         oc = MoveitOrientationConstraint()
-        oc.header = self.GenEEHeader()
+        # oc.header = self.GenEEHeader()
+        oc.header = pose.header
+        oc.link_name = self.EE_LINK_NAME
 
         # (roll about an X-axis)
         # (subsequent pitch about the Y-axis)
@@ -122,35 +148,38 @@ class EEPlaner(RosNode):
         # TODO Who's xyz is Moveit's orientation constrain with? EE or base?
         # I think it's EE. regardless, our WX200 define X out, Y left, Z up.
 
-        qw, qx, qy , qz = quaternion_from_euler(0, 0, z_ang)
+        z_ang = math.atan2(pose.pose.position.y,pose.pose.position.x)
+        print(f"Doing Z ang {z_ang}")
+        qx, qy , qz,qw = quaternion_from_euler(0, 0, z_ang)
 
         # The two angle we want flat will just get about 45 deg tolerance?
         # TODO consider lower these values
-        oc.absolute_x_axis_tolerance = math.pi 
-        oc.absolute_y_axis_tolerance = math.pi 
-        oc.absolute_z_axis_tolerance = 2 * math.pi
+        oc.absolute_x_axis_tolerance = math.pi
+        oc.absolute_y_axis_tolerance = math.pi
+        oc.absolute_z_axis_tolerance = math.pi
         oc.orientation.w =qw
         oc.orientation.x =qx
         oc.orientation.y =qy
         oc.orientation.z =qz
+        oc.parameterization = MoveitOrientationConstraint.XYZ_EULER_ANGLES
         # orientation is really don't care much
-        oc.weight = 0.4
+        oc.weight = 0.5
         return oc
 
-    def GetPosConstrain(self, pose: PoseMsg):
+    def GetPosConstrain(self, pose: PoseStamped):
         # https://github.com/ros-planning/moveit_msgs/blob/ros2/msg/PositionConstraint.msg
         pc = MoveitPositionConstraint()
-        pc.header = self.GenEEHeader()
-
+        pc.header = pose.header
         pc.link_name = self.EE_LINK_NAME
         bv = BoundingVolume()
         sp = SolidPrimitive()
 
         sp.type = SolidPrimitive.SPHERE
-        sp.dimensions = [100 / 1000]  # 5mm of sphere toleranec
+        sp.dimensions = [5 / 1000]  # 5mm of sphere toleranec
         bv.primitives = [sp]
-        bv.primitive_poses = [pose]
+        bv.primitive_poses = [pose.pose]
         pc.constraint_region = bv
+        pc.weight= 0.5
         return pc
 
 
