2723f116a5edf43cb8141e2b4660568563ebc260
leo-chen <S8O9R6@u.northwestern.edu>
2024-03-05 11:33:38 -0600

    Add a set of no discover server setup script

--- interbotics-ws/LoCoBot_no_discover.sh
+++ interbotics-ws/LoCoBot_no_discover.sh
@@ -5,9 +5,7 @@
 # all zsh to bash
 set -xv
 here="$(dirname $(realpath $0))"
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 # export FASTRTPS_DEFAULT_PROFILES_FILE="${here}/src/interbotix_ros_rovers/interbotix_ros_xslocobots/install/resources/super_client_configuration_file.xml"
-# export ROS_DISCOVERY_SERVER=127.0.0.1:11811
 export INTERBOTIX_XSLOCOBOT_BASE_TYPE=create3
 
 source /opt/ros/humble/setup.zsh
